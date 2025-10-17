9535184e0976e39fe8512d6d8801ccedc203b0d3
Rafael Slobodian <rafael@withmantle.com>
2025-10-17 14:44:49 -0400

    feat!: Add ability to pass in paginated text content or binary file content into the context
    - Breaking interface change with a new FileContext model that wraps all the file data to be put into the prompt context
    - Allows the user to pass in paginated text content, which indicates in the context which parts of the text came from which page numbers
    - Allows the user to pass in binary content, to allow for multi-modal prompting with raw PDFs
    - Errors out if file content is provided but not a category

--- generate.go
+++ generate.go
@@ -7,11 +7,33 @@ import (
 	"google.golang.org/genai"
 )
 
+type FileContext struct {
+	FileCategory string
+
+	// pass one of
+	BinaryFileContent     BinaryFileContent      // For Raw PDFs and other file types (multi-modal prompting)
+	FileContent           string                 // For text-based files (non-page-delineated)
+	PaginatedFileContents []PaginatedFileContent // For paginated files (e.g., multi-page PDFs extracted to text)
+}
+
+func (fc FileContext) IsEmpty() bool {
+	return fc.FileContent == "" && len(fc.PaginatedFileContents) == 0 && len(fc.BinaryFileContent.FileBytes) == 0
+}
+
+type PaginatedFileContent struct {
+	PageNumber  int
+	PageContent string
+}
+
+type BinaryFileContent struct {
+	FileBytes    []byte
+	FileMimeType string
+}
+
 type PromptParams struct {
 	SystemInstructions []string
 	Prompt             []string
-	FileCategory       string
-	FileContent        string
+	FileContext        FileContext
 	ResponseStruct     any
 	TemplateVariables  map[string]string
 }
@@ -38,10 +60,12 @@ func GenerateGeminiParts(params PromptParams) (*genai.Content, []*genai.Part, *g
 
 	var promptParts []*genai.Part
 
-	if params.FileCategory != "" && params.FileContent != "" {
-		promptParts = append(promptParts, genai.NewPartFromText(fmt.Sprintf("--- %s\n\n", params.FileCategory)))
-		promptParts = append(promptParts, genai.NewPartFromText(params.FileContent))
-		promptParts = append(promptParts, genai.NewPartFromText("\n\n---\n\n"))
+	if !params.FileContext.IsEmpty() {
+		var err error
+		promptParts, err = generateFileContextParts(params.FileContext)
+		if err != nil {
+			return nil, nil, nil, err
+		}
 	}
 
 	for _, prompt := range params.Prompt {
@@ -54,4 +78,54 @@ func GenerateGeminiParts(params PromptParams) (*genai.Content, []*genai.Part, *g
 	}
 
 	return systemInstruction, promptParts, responseSchema, nil
+}
+
+func generateFileContextParts(fileContext FileContext) ([]*genai.Part, error) {
+	if fileContext.FileCategory == "" {
+		return nil, fmt.Errorf("fileCategory is required")
+	}
+
+	var promptParts []*genai.Part
+	switch {
+	case fileContext.FileContent != "":
+		promptParts = append(promptParts, generateFileContentParts(fileContext.FileContent, fileContext.FileCategory)...)
+	case len(fileContext.PaginatedFileContents) > 0:
+		promptParts = append(promptParts, generatePaginatedFileContentParts(fileContext.PaginatedFileContents, fileContext.FileCategory)...)
+	case len(fileContext.BinaryFileContent.FileBytes) > 0:
+		promptParts = append(promptParts, generateFileBytesParts(fileContext.BinaryFileContent, fileContext.FileCategory)...)
+	default:
+		return nil, fmt.Errorf("file content not specified")
+	}
+
+	return promptParts, nil
+}
+
+func generateFileContentParts(fileContent string, fileCategory string) []*genai.Part {
+	var parts []*genai.Part
+	if fileCategory != "" && fileContent != "" {
+		parts = append(parts, genai.NewPartFromText(fmt.Sprintf("--- %s\n\n", fileCategory)))
+		parts = append(parts, genai.NewPartFromText(fileContent))
+		parts = append(parts, genai.NewPartFromText("\n\n---\n\n"))
+	}
+	return parts
+}
+
+func generatePaginatedFileContentParts(paginatedFileContents []PaginatedFileContent, fileCategory string) []*genai.Part {
+	var parts []*genai.Part
+	parts = append(parts, genai.NewPartFromText(fmt.Sprintf("--- %s\n\n", fileCategory)))
+	for _, page := range paginatedFileContents {
+		parts = append(parts, genai.NewPartFromText(fmt.Sprintf("------ (Page %d)\n\n", page.PageNumber)))
+		parts = append(parts, genai.NewPartFromText(page.PageContent))
+		parts = append(parts, genai.NewPartFromText("\n\n------\n\n"))
+	}
+	parts = append(parts, genai.NewPartFromText("\n\n---\n\n"))
+	return parts
+}
+
+func generateFileBytesParts(binaryFile BinaryFileContent, fileCategory string) []*genai.Part {
+	var parts []*genai.Part
+	parts = append(parts, genai.NewPartFromText(fmt.Sprintf("--- %s\n\n", fileCategory)))
+	parts = append(parts, genai.NewPartFromBytes(binaryFile.FileBytes, binaryFile.FileMimeType))
+	parts = append(parts, genai.NewPartFromText("\n\n---\n\n"))
+	return parts
 }
